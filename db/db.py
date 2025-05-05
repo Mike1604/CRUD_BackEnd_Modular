5fa7014e661363eb1cd4cab724248aac330158e6
Efrain Palacios <soft.pal.ef@gmail.com>
2025-05-05 09:56:53 -0600

    changes in api

--- db/db.py
+++ db/db.py
@@ -1,13 +1,11 @@
 from pymongo import MongoClient
 
-
 def get_db_client():
     #Uncomment the db you want to use 
-    #uri = "mongodb://localhost:27017/"
-    uri = "mongodb+srv://Modular:FlashCardsModular@cluster0.mresw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    uri = "mongodb://localhost:27017/"
+    #uri = "mongodb+srv://Modular:FlashCardsModular@cluster0.mresw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     conn = MongoClient(uri);
     return conn;
-
 
 conn = get_db_client();
 db = conn.get_database("modular_db");
