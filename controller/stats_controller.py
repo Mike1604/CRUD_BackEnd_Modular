2ba4125ce0fe2bda30735d66440bd558ab3caf79
Mike1604 <abraham.flores4433@alumnos.udg.mx>
2025-05-04 22:00:57 -0600

    [feat]Adding group stats

--- controller/stats_controller.py
+++ controller/stats_controller.py
@@ -1,8 +1,21 @@
 from fastapi import HTTPException
 from db.db import db
-from models.stats_model import StudySession;
+from models.stats_model import GroupStudySession, StudySession;
 
 statsCollection = db.get_collection("stats")
+groupStatsCollection = db.get_collection("group_stats")
+
+def get_all_study_sessions():
+    try:
+        stats = list(statsCollection.find())
+
+        for stat in stats:
+            stat["id"] = str(stat["_id"])  
+            del stat["_id"]
+        return stats
+    except Exception as e:
+        print(f"Error getting all study sessions: {e}")
+        raise
 
 def get_study_sessions(userId: str):
     try:
@@ -16,9 +29,31 @@ def get_study_sessions(userId: str):
         print(f"Error getting study sessions: {e}")
         raise
 
-def record_study_session(data : StudySession):
+def record_study_session(data : StudySession, userId : str):
     session_doc = data.model_dump()
+    session_doc["user_id"] = userId
     insert_result = statsCollection.insert_one(session_doc)
 
     if not insert_result.acknowledged:
         raise HTTPException(status_code=500, detail="Failed to save study session")
+
+def get_group_study_sessions(groupId:str):
+    try:
+        stats = list(groupStatsCollection.find({"group_id": groupId}))
+
+        for stat in stats:
+            stat["id"] = str(stat["_id"])  
+            del stat["_id"]
+        return stats
+    except Exception as e:
+        print(f"Error getting study sessions: {e}")
+        raise
+
+def record_group_study_session(data : GroupStudySession, userId : str):
+    session_doc = data.model_dump()
+    session_doc["user_id"] = userId
+    insert_result = groupStatsCollection.insert_one(session_doc)
+
+    if not insert_result.acknowledged:
+        raise HTTPException(status_code=500, detail="Failed to save group study session")
+
--- models/stats_model.py
+++ models/stats_model.py
@@ -2,7 +2,17 @@ from pydantic import BaseModel
 from datetime import datetime
 
 class StudySession(BaseModel):
-    user_id: str
+    deck: str
+    language: str
+    session_duration_minutes: float
+    flashcards_studied: int 
+    correct_answers: int 
+    incorrect_answers: int 
+    session_date: datetime 
+
+class GroupStudySession(BaseModel):
+    group_id: str
+    activity: str
     deck: str
     language: str
     session_duration_minutes: float
